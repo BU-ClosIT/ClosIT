75ff2082917effd6d7067bd27e09f319995e2d65
Nagl-J <jasonfong8@gmail.com>
2025-10-06 20:25:49 -0400

    ignore this

---
+++ frontend/src/model/firebaseConfig.ts
@@ -0,0 +1,37 @@
+import { initializeApp } from "firebase/app";
+import { getFirestore } from "firebase/firestore";
+
+import {
+  getAI,
+  GoogleAIBackend,
+  getGenerativeModel, // Make sure getGenerativeModel is imported!
+  GenerativeModel // Make sure GenerativeModel (the type) is imported!
+} from "firebase/ai"; // This is the correct package for the Firebase AI SDK for web
+
+
+const firebaseConfig = {
+
+  apiKey: "AIzaSyDSKHxlK2sSpGtwSjsABMs3m4eJ5uyaI68",
+  authDomain: "closit-db-test.firebaseapp.com",
+  projectId: "ClosIT-db-test",
+  storageBucket: "closit-db-test.firebasestorage.app",
+  messagingSenderId: "500170850853",
+  appId: "1:500170850853:web:41f46138e7221025fe35cc"
+};
+
+
+// Initialize Firebase
+export const app = initializeApp(firebaseConfig);
+
+// Export Firestore instance
+export const db = getFirestore(app);
+
+// Initialize the Gemini Developer API backend service
+
+const firebaseAI = getAI(app, {backend: new GoogleAIBackend()})
+
+const aiServiceInstance = getAI(app, { backend: new GoogleAIBackend() });
+
+export const aiModel: GenerativeModel = getGenerativeModel(aiServiceInstance, {
+  model: "gemini-1.5-flash", // Or 'gemini-pro'
+});
