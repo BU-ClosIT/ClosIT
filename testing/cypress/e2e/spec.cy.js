7bbfbc30ea445d32d57244f0f689681439262800
Dhruv Prajapati <dhruv16@bu.edu>
2025-11-20 18:18:53 -0500

    done testing for rendering logo and navbar

---
+++ testing/cypress/e2e/spec.cy.js
@@ -0,0 +1,40 @@
+describe('cypress demo', () => {
+  it('renders logo on the screen', () => {
+    cy.visit('http://localhost:3000/dashboard')
+    
+    // check if the logo is present and check if the text is correct
+    cy.get('[data-testid="cypress-logo"]')
+    .should('exist')
+    .should('have.text', 'ClosIT')
+  })
+
+  it('renders navbar on the screen', () => {
+    cy.visit('http://localhost:3000/dashboard')
+
+    // check if the dashboard link is present and check its text is correct
+    cy.get('[data-testid="cypress-nav-dashboard-link"]')
+    .should('exist')
+    .should('have.text', 'Dashboard')
+
+    // check if the manage closet link is present and check its text is correct
+    cy.get('[data-testid="cypress-nav-closet-link"]')
+    .should('exist')
+    .should('have.text', 'Manage Closet')
+  })
+
+  it('renders current weather on the screen', () => {
+    cy.visit('http://localhost:3000/dashboard')
+
+    // check if the datetime is present
+    cy.get('[data-testid="cypress-currentweather-datetime"]')
+    .should('exist')
+
+    // check if the location is present
+    cy.get('[data-testid="cypress-currentweather-location"]')
+    .should('exist')
+
+    // check if the temperature is present
+    cy.get('[data-testid="cypress-currentweather-temp"]')
+    .should('exist')
+  })
+}) 
