5316eac9a3de22f41cac83f1b5f1bf354dd5b694
Dhruv Prajapati <dhruv16@bu.edu>
2025-11-21 09:47:09 -0500

    did some change-(ids in frontend components) for unit tests

--- frontend/src/components/shared/Logo.tsx
+++ frontend/src/components/shared/Logo.tsx
@@ -3,7 +3,7 @@ import Link from "next/link";
 export default function Logo() {
   return (
     <div className="text-2xl font-bold hover:scale-115">
-      <Link href="/dashboard">ClosIT</Link>
+      <Link href="/dashboard" data-testid="cypress-logo">ClosIT</Link>
     </div>
   );
 }
